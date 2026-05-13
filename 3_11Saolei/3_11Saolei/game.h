6e6976a950b4ef6be951dfbf37345d3ca35247cf
yyCharlotte <2295608472@qq.com>
2026-05-13 17:49:38 +0800

    扫雷的代码

---
+++ 3_11Saolei/3_11Saolei/game.h
@@ -0,0 +1,18 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define EASY_COUNT 10
+#define ROW 9
+#define COL 9
+#define ROWS ROW+2
+#define COLS COL+2
+//≥ı ºªØ∆Â≈Ã
+void InitBoard(char board[ROWS][COLS], int rows, int cols, char set);
+//¥Ú”°∆Â≈Ã
+void DisplayBoard(char board[ROWS][COLS], int row, int col);
+//≤º÷√¿◊
+void SetMine(char board[ROWS][COLS], int row, int col);
+//≈≈≤È¿◊
+void FindMine(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int col);
